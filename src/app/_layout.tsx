2b91f06191e28475736304a5c203a59900e36999
Esteban Escobar <esteban.escobar@gorillalogic.com>
2025-06-08 13:09:41 -0500

    Add Convex integration and Clerk backend support; update dependencies and add sample data

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,61 @@
-import { Stack } from "expo-router";
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  useFonts,
+} from "@expo-google-fonts/poppins";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { Slot, SplashScreen } from "expo-router";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import "../global.css";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+// Prevent auto hide splash screen until fonts are loaded
+SplashScreen.preventAutoHideAsync();
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
+
+const InitialLayout = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Slot />;
+};
+
+const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  return (
+    <ConvexProvider client={convex}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+          <InitialLayout />
+        </ThemeProvider>
+      </GestureHandlerRootView>
+    </ConvexProvider>
+  );
+};
+
+export default RootLayout;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,17 @@
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const tasks = useQuery(api.tasks.get);
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 items-center justify-center bg-dark"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {tasks?.map((task) => (
+        <Text className="text-white" key={task._id}>{task.text}</Text>
+      ))}
     </View>
   );
 }
